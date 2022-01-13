916a166f323fc50a129176337e9fea2137709294
koestardono <kustar251096@gmail.com>
2022-01-13 15:54:27 +0700

    tes

---
+++ src/main/java/train/perpusbackend/PerpusBackendApplication.java
@@ -0,0 +1,13 @@
+package train.perpusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerpusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerpusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/train/perpusbackend/PerpusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package train.perpusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerpusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
